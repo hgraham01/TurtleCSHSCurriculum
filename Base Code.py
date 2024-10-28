492d11a539fa13d46af1db903fb16bc8971e188e
hgraham01 <hgraham01@student.dasd.org>
2024-10-28 00:11:34 -0400

    Update Base Code.py
    ### Changed base code to make lesson easier
    - Changed it to start by moving up
    - Increased speed to 2

--- Base Code.py
+++ Base Code.py
@@ -2,6 +2,7 @@
 from turtle import *
 # --------------------------
 
-speed(1) # Modify the number to increase the speed of the turtle
+speed(2) # Modify the number to increase the speed of the turtle
 color("red") # Use to modify the color, choose any common color name
+left(90) # Makes the turtle face up to start
 # Your code goes below this line
